4a5ac2dce81f76a0ef4455ca767f9ba8da26f0bf
xibo.826 <xibo.826@gamil.com>
2021-01-29 23:02:41 +0900

    11

--- src/test/java/com/spring/sample/UserTest.java
+++ src/test/java/com/spring/sample/UserTest.java
@@ -5,22 +5,39 @@ import com.spring.sample.domain.User;
 import com.spring.sample.service.IUserService;
 import config.SpringConfiguar;
 import org.junit.Test;
+import org.junit.internal.Classes;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
+/**
+ * Spring整合Junit
+ * @RunWith:
+ *       用Spring的Runner替换Junit的Runner
+ * @ContextConfiguration:
+ *       指定Ico容器初始化时的配置文件或者配置Class
+ *    classes:配置Class
+ *    locations:配置文件地址 如:classpath:context/bean.xml
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfiguar.class)
 public class UserTest {
+
+    @Autowired
+    private IUserService userService;
 
     @Test
     public void testFindAll() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguar.class);
-        IUserService userService = ac.getBean("userService", IUserService.class);
 
         List<User> users = userService.findAll();
-
         for(User user : users) {
             System.out.println(user);
         }
